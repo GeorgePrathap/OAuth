2ab479a2a71832b657fa864e5f18620ea9023c61
george <george@mail.com>
2023-11-28 17:36:54 +0530

    Google OAuth inprogress

--- go.mod
+++ go.mod
@@ -4,12 +4,14 @@ go 1.18
 
 require (
 	github.com/gorilla/mux v1.8.1
-	golang.org/x/oauth2 v0.14.0
+	golang.org/x/oauth2 v0.15.0
 )
 
 require (
+	cloud.google.com/go/compute v1.20.1 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
--- server.go
+++ server.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
-	// "golang.org/x/oauth2"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+var (
+	clientID     = ""
+	clientSecret = ""
+	redirectURL  = "http://localhost:8080/google/callback"
+	scopes       = []string{"https://www.googleapis.com/auth/plus.login"}
+
+	googleConfig = oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectURL,
+		Scopes:       scopes,
+		Endpoint:     google.Endpoint,
+	}
 )
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/home", handleHome)
 	r.HandleFunc("/google/login", handleGoogleLogin)
+	r.HandleFunc("/google/callback", handleGoogleCallBack)
 	r.HandleFunc("/facebook/login", handleFacebookLogin)
 
 	http.Handle("/", r)
 
-	fmt.Println("Server started on http://localhost:8080")
+	log.Println("Server started on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -27,7 +46,54 @@ func handleHome(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
-	log.Println("Inside google login")
+	url := googleConfig.AuthCodeURL("state", oauth2.AccessTypeOffline)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func handleGoogleCallBack(w http.ResponseWriter, r *http.Request) {
+	log.Println("inside callback ...")
+
+	// Retrieve the authorization code from the query parameters
+	code := r.URL.Query().Get("code")
+	if code == "" {
+		log.Println("Authorization code not found")
+		http.Error(w, "Authorization code not found", http.StatusBadRequest)
+		return
+	}
+
+	// Exchange the authorization code for an access token
+	token, err := googleConfig.Exchange(context.Background(), code)
+	if err != nil {
+		log.Printf("Failed to exchange token: %s\n", err)
+		http.Error(w, fmt.Sprintf("Failed to exchange token: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	// Now you have the access token, and you can use it to make API requests.
+	// For example, you can print the user's profile information.
+	client := googleConfig.Client(context.Background(), token)
+	resp, err := client.Get("https://www.googleapis.com/plus/v1/people/me")
+	if err != nil {
+		log.Printf("Failed to get user profile: %s\n", err)
+		http.Error(w, fmt.Sprintf("Failed to get user profile: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var profile map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&profile)
+	if err != nil {
+		log.Printf("Failed to parse JSON response: %s\n", err)
+		http.Error(w, fmt.Sprintf("Failed to parse JSON response: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	log.Println("working")
+
+	for key, value := range profile {
+		log.Printf("%v : %v\n", key, value)
+	}
 }
 
 func handleFacebookLogin(w http.ResponseWriter, r *http.Request) {
